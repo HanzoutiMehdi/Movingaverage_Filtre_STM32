13f5e9c2f91d899987c1c0ee4e8dfc92a8aec85d
MHANZOUTI <mehdi.hanzouti@actia-engineering.tn>
2021-12-27 02:22:12 +0100

    add more filtre

--- Core/Src/task_process.c
+++ Core/Src/task_process.c
@@ -24,8 +24,26 @@
 #include "usb_device.h"
 #include "stdlib.h"
 #include "myfir.h"
+#include "iir_filtre.h"
+#include "notsh_filtre.h"
 
 
+/*NOTCH FILTRE*/
+NotchFiltre  hnotch;
+#define SAMPLE_TIME_MS_TASK            10 /*ms*/
+
+#define NOTCH_FILTRE_CENTER_HZ         5.1f
+#define NOTCH_FILTRE_WIDTH_HZ          0.1f
+#define NOTCH_FILTRE_SAMPLETIME_S      (0.001f* SAMPLE_TIME_MS_TASK) /*ms to sencoed*/
+
+
+
+/*IIR FILTRE */
+IIRFiltre   hiir;
+#define APLFA_IIR    0.5f
+
+
+/*FIR FILTRE*/
 FIRFiltre lpfAcc;
 
 void vmainTask(void const * argument);
@@ -33,10 +51,16 @@ uint8_t float_toString(float value, char *ch, uint8_t type);
 uint8_t float_toString_dual(float value,float value2 ,char *ch);
 uint8_t SerialPrint_Value(float value, uint8_t type);
 uint8_t SerialPrint_DualValue(float value1,float value2);
+uint8_t SerialPrint_ThriValue(float value1,float value2,float value3);
+uint8_t float_toString_Third(float value,float value2 ,float value3,char *ch);
 
 #define WAIT_NEXT_VALUE            1U
 #define PRINT_VALUE                0U
 int16_t  Acc[3];
+
+uint8_t test1, test2;
+float inputFreq_Hz=1.0f ;/*2Hz*/
+
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
@@ -52,23 +76,48 @@ void vmainTask(void const * argument)
 
 
 
-
+    /*Init FIR*/
 	FIRFiltre_Init(&lpfAcc);
 
+    /*Init IIR*/
+	IIRFiltre_Init(&hiir,APLFA_IIR);
 
-
+    /*Notch filtre init*/
+	NotchFiltre_Init(&hnotch,NOTCH_FILTRE_CENTER_HZ,NOTCH_FILTRE_WIDTH_HZ, NOTCH_FILTRE_SAMPLETIME_S);
 
 
 	while(1)
 	{
 
-      osDelay(10U); /*100hz*/
+      osDelay(SAMPLE_TIME_MS_TASK); /*100hz*/
 
       (void)BSP_ACCELERO_GetXYZ(Acc);
-
+      /*FIR update*/
       FIRFiltre_Update(&lpfAcc, Acc[1]);
+      /*FIR update*/
+      IIRFiltre_Update(&hiir, Acc[1]);
 
-      SerialPrint_DualValue(Acc[1],lpfAcc.out);
+
+      float inputSin = 100.0f*sin(2.0f*M_PI*inputFreq_Hz*(0.001f*HAL_GetTick() ));
+      float notch_out=NotchFiltre_Update(&hnotch,inputSin);
+
+      /*Notch filtre update*/
+      //NotchFiltre_Update(&hnotch,Acc[1]);
+
+      SerialPrint_DualValue(inputSin,notch_out);
+
+      //SerialPrint_DualValue(Acc[1],lpfAcc.out);
+   if (test1)
+   {
+	   lpfAcc.out=0;
+   }
+   if (test2)
+   {
+	   hiir.out=0;
+   }
+
+    //  SerialPrint_ThriValue(Acc[1],lpfAcc.out,hiir.out);
+
 
 
 #if 0
@@ -134,6 +183,40 @@ uint8_t SerialPrint_DualValue(float value1,float value2)
 }
 
 
+/**
+  * @brief  Serial Print to Oscilo
+  * @retval type: 0 print stored value
+  *             : 1 store value and not print, wait for other value to be sent
+  */
+uint8_t SerialPrint_ThriValue(float value1,float value2,float value3)
+{
+#define CH_LENGHT          100U
+	uint8_t ret;
+	static char ch[CH_LENGHT];
+
+	/*Clear ch*/
+	for (uint8_t i=0;i<CH_LENGHT;i++)
+	{
+		ch[i]=0;
+	}
+
+	/*float to string on shot value :print only one value*/
+	ret=float_toString_Third(value1,value2,value3,ch);
+	if (ret !=0)
+	{
+		return 1;
+	}
+
+	uint16_t len=strlen(ch);
+
+	/*send buffer to USB CDC class */
+	ret=CDC_Transmit_FS(ch,len);
+
+
+
+	return ret;
+
+}
 
 
 
@@ -177,6 +260,51 @@ uint8_t SerialPrint_Value(float value, uint8_t type)
 	return ret;
 
 }
+
+
+uint8_t float_toString_Third(float value,float value2 ,float value3,char *ch)
+{
+	char *tmpSign = (value < 0) ? "-" : "";
+	float tmpVal = (value < 0) ? -value : value;
+
+	int tmpInt1 = tmpVal;                  // Get the integer
+	float tmpFrac = tmpVal - tmpInt1;      // Get fraction
+	int tmpInt2 = trunc(tmpFrac * 10000);  // Turn into integer
+
+
+	/*value2*/
+	char *tmpSign2 = (value2 < 0) ? "-" : "";
+	float tmpVal2 = (value2 < 0) ? -value2 : value2;
+
+	int tmpInt1_2 = tmpVal2;                  // Get the integer
+	float tmpFrac2 = tmpVal2 - tmpInt1_2;      // Get fraction
+	int tmpInt2_2 = trunc(tmpFrac2 * 10000);  // Turn into integer
+
+    /*value 3*/
+	char *tmpSign3 = (value3 < 0) ? "-" : "";
+	float tmpVal3 = (value3 < 0) ? -value3 : value3;
+
+	int tmpInt1_3 = tmpVal3;                  // Get the integer
+	float tmpFrac3 = tmpVal3 - tmpInt1_3;      // Get fraction
+	int tmpInt2_3 = trunc(tmpFrac3 * 10000);  // Turn into integer
+
+
+
+
+	if ((tmpInt2<10000U) && (tmpInt2_2<10000U)&& (tmpInt2_3<10000U))
+    {
+
+	 sprintf (ch, "%s%d.%04d,%s%d.%04d,%s%d.%04d\r\n", tmpSign, tmpInt1, tmpInt2, tmpSign2, tmpInt1_2, tmpInt2_2,
+			  			                               tmpSign3, tmpInt1_3, tmpInt2_3 );
+	return 0;
+    }
+	else
+	{
+		return 1; /*so big value to convert*/
+	}
+}
+
+
 
 
 uint8_t float_toString_dual(float value,float value2 ,char *ch)
@@ -259,4 +387,5 @@ void assert_failed(uint8_t *file, uint32_t line)
 #endif /* USE_FULL_ASSERT */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
 
